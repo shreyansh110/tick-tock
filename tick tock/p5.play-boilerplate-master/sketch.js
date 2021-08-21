912f07f655e29d313b0a41ec6f90d2165b175d83
shreyansh110 <69755759+shreyansh110@users.noreply.github.com>
2021-08-21 19:29:03 +0530

    Add files via upload

---
+++ tick tock/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,84 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+const Body=Matter.Body;
+
+//calculating time using predefined functions from p5.js
+ //var hr,mn,sc
+ 
+ var hr,mn,sc;
+
+   
+ 
+
+
+function preload(){
+
+}
+
+function setup() {
+  createCanvas(800,400);
+
+  engine=Engine.create();
+  world=engine.world;
+
+  angleMode(DEGREES);
+ //createSprite(400, 200, 50, 50);
+
+}
+
+function draw() {
+  background(0,0,0); 
+  Engine.update(engine); 
+
+  translate(400,200);
+
+rotate(-90);
+  hr=hour();
+  mn=minute();
+  sc=second();
+
+console.log(sc);
+
+
+  scAngle=map(sc,0,60,0,360);
+  mnAngle=map(mn,0,60,0,360);
+  hrAngle=map(hr%12,0,12,0,360);
+
+
+push();
+
+rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,50,110);
+  pop();
+  push();
+  rotate(mnAngle);
+  stroke(0,255,0);
+  strokeWeight(7);
+  line(0,0,50,80);
+  pop();
+  push();
+  rotate(hrAngle)
+  stroke(0,0,255);
+  strokeWeight(7);
+  line(0,0,50,50);
+pop();
+
+noFill();
+strokeWeight(10);
+stroke(255,0,0);
+arc(0,0,300,300,0,scAngle)
+
+noFill();
+strokeWeight(10);
+stroke(0,255,0);
+arc(0,0,280,280,0,mnAngle);
+
+noFill();
+strokeWeight(10);
+stroke(0,0,255);
+arc(0,0,260,260,0,hrAngle);
+  drawSprites();
+}
